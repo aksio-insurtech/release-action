1d9eafdf748626ef3dbd1ac4be61b8fdc74d1890
Einar Ingebrigtsen <einari@me.com>
2021-09-10 14:59:39 +0200

    %1

--- Source/index.ts
+++ Source/index.ts
@@ -11,78 +11,82 @@ const octokit = new Octokit({ auth: inputs.gitHubToken });
 
 run();
 async function run() {
+    try {
 
-    const mergedPr = await getMergedPr(context.repo.owner, context.repo.repo, context.sha);
-    if (!mergedPr) {
-        logger.error('No merged PR found.');
-        return;
-    }
-    if (mergedPr.labels.length === 0) {
-        logger.info('No release labels found.');
-        outputs.setShouldPublish(false);
-        return;
-    }
-
-    let isMinor = false;
-    let isPatch = false;
-    const isMajor = mergedPr.labels.find(_ => _.name === 'major');
-    if (!isMajor) {
-        isMinor = mergedPr.labels.some(_ => _.name === 'minor');
-        if (!isMinor) {
-            isPatch = mergedPr.labels.some(_ => _.name === 'patch');
+        const mergedPr = await getMergedPr(context.repo.owner, context.repo.repo, context.sha);
+        if (!mergedPr) {
+            logger.error('No merged PR found.');
+            return;
         }
+        if (mergedPr.labels.length === 0) {
+            logger.info('No release labels found.');
+            outputs.setShouldPublish(false);
+            return;
+        }
+
+        let isMinor = false;
+        let isPatch = false;
+        const isMajor = mergedPr.labels.find(_ => _.name === 'major');
+        if (!isMajor) {
+            isMinor = mergedPr.labels.some(_ => _.name === 'minor');
+            if (!isMinor) {
+                isPatch = mergedPr.labels.some(_ => _.name === 'patch');
+            }
+        }
+
+        if (!isMinor && !isMinor && !isPatch) {
+            logger.info('No release related labels associated with the PR.');
+            return;
+        }
+
+        let latestTag = await getLatestTag(
+            octokit,
+            context.repo.owner,
+            context.repo.repo,
+            true,
+            'v',
+            '',
+            true);
+
+        if (latestTag.toLowerCase().startsWith('v')) {
+            latestTag = latestTag.substr(1);
+        }
+
+        logger.info(`Latest tag: ${latestTag}`);
+
+
+        let version = semver.parse(latestTag);
+        if (!version) {
+            logger.error(`Version string '${latestTag}' is not in a valid format`);
+            return;
+        }
+        if (isMajor) version = version.inc('major') || version;
+        if (isMinor) version = version.inc('minor') || version;
+        if (isPatch) version = version.inc('patch') || version;
+
+        logger.info(`New version is '${version.version}''`);
+
+        await octokit.repos.createRelease({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            tag_name: `v${version.version}`,
+            name: mergedPr.title,
+            body: mergedPr.body || '',
+            prerelease: false,
+            target_commitish: context.sha
+        });
+
+        logger.info('GitHub release created');
+
+        await prependToChangeLog(mergedPr.body || '', `v${version.version}`, mergedPr.url);
+
+        logger.info('Prepended to changelog');
+
+        outputs.setVersion(version.version);
+        outputs.setShouldPublish(true);
+    } catch (ex ) {
+        logger.error("Something went wrong", ex);
     }
-
-    if (!isMinor && !isMinor && !isPatch) {
-        logger.info('No release related labels associated with the PR.');
-        return;
-    }
-
-    let latestTag = await getLatestTag(
-        octokit,
-        context.repo.owner,
-        context.repo.repo,
-        true,
-        'v',
-        '',
-        true);
-
-    if (latestTag.toLowerCase().startsWith('v')) {
-        latestTag = latestTag.substr(1);
-    }
-
-    logger.info(`Latest tag: ${latestTag}`);
-
-
-    let version = semver.parse(latestTag);
-    if (!version) {
-        logger.error(`Version string '${latestTag}' is not in a valid format`);
-        return;
-    }
-    if (isMajor) version = version.inc('major') || version;
-    if (isMinor) version = version.inc('minor') || version;
-    if (isPatch) version = version.inc('patch') || version;
-
-    logger.info(`New version is '${version.version}''`);
-
-    await octokit.repos.createRelease({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        tag_name: `v${version.version}`,
-        name: mergedPr.title,
-        body: mergedPr.body || '',
-        prerelease: false,
-        target_commitish: context.sha
-    });
-
-    logger.info('GitHub release created');
-
-    await prependToChangeLog(mergedPr.body || '', `v${version.version}`, mergedPr.url);
-
-    logger.info('Prepended to changelog');
-
-    outputs.setVersion(version.version);
-    outputs.setShouldPublish(true);
 }
 
 
--- dist/index.js
+++ dist/index.js
@@ -31264,60 +31264,65 @@ async function pushChanges() {
 const octokit = new dist_node/* Octokit */.v({ auth: inputs.gitHubToken });
 run();
 async function run() {
-    const mergedPr = await getMergedPr(github.context.repo.owner, github.context.repo.repo, github.context.sha);
-    if (!mergedPr) {
-        logger.error('No merged PR found.');
-        return;
-    }
-    if (mergedPr.labels.length === 0) {
-        logger.info('No release labels found.');
-        outputs.setShouldPublish(false);
-        return;
-    }
-    let isMinor = false;
-    let isPatch = false;
-    const isMajor = mergedPr.labels.find(_ => _.name === 'major');
-    if (!isMajor) {
-        isMinor = mergedPr.labels.some(_ => _.name === 'minor');
-        if (!isMinor) {
-            isPatch = mergedPr.labels.some(_ => _.name === 'patch');
+    try {
+        const mergedPr = await getMergedPr(github.context.repo.owner, github.context.repo.repo, github.context.sha);
+        if (!mergedPr) {
+            logger.error('No merged PR found.');
+            return;
         }
+        if (mergedPr.labels.length === 0) {
+            logger.info('No release labels found.');
+            outputs.setShouldPublish(false);
+            return;
+        }
+        let isMinor = false;
+        let isPatch = false;
+        const isMajor = mergedPr.labels.find(_ => _.name === 'major');
+        if (!isMajor) {
+            isMinor = mergedPr.labels.some(_ => _.name === 'minor');
+            if (!isMinor) {
+                isPatch = mergedPr.labels.some(_ => _.name === 'patch');
+            }
+        }
+        if (!isMinor && !isMinor && !isPatch) {
+            logger.info('No release related labels associated with the PR.');
+            return;
+        }
+        let latestTag = await getLatestTag(octokit, github.context.repo.owner, github.context.repo.repo, true, 'v', '', true);
+        if (latestTag.toLowerCase().startsWith('v')) {
+            latestTag = latestTag.substr(1);
+        }
+        logger.info(`Latest tag: ${latestTag}`);
+        let version = semver_default().parse(latestTag);
+        if (!version) {
+            logger.error(`Version string '${latestTag}' is not in a valid format`);
+            return;
+        }
+        if (isMajor)
+            version = version.inc('major') || version;
+        if (isMinor)
+            version = version.inc('minor') || version;
+        if (isPatch)
+            version = version.inc('patch') || version;
+        logger.info(`New version is '${version.version}''`);
+        await octokit.repos.createRelease({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            tag_name: `v${version.version}`,
+            name: mergedPr.title,
+            body: mergedPr.body || '',
+            prerelease: false,
+            target_commitish: github.context.sha
+        });
+        logger.info('GitHub release created');
+        await prependToChangeLog(mergedPr.body || '', `v${version.version}`, mergedPr.url);
+        logger.info('Prepended to changelog');
+        outputs.setVersion(version.version);
+        outputs.setShouldPublish(true);
     }
-    if (!isMinor && !isMinor && !isPatch) {
-        logger.info('No release related labels associated with the PR.');
-        return;
+    catch (ex) {
+        logger.error("Something went wrong", ex);
     }
-    let latestTag = await getLatestTag(octokit, github.context.repo.owner, github.context.repo.repo, true, 'v', '', true);
-    if (latestTag.toLowerCase().startsWith('v')) {
-        latestTag = latestTag.substr(1);
-    }
-    logger.info(`Latest tag: ${latestTag}`);
-    let version = semver_default().parse(latestTag);
-    if (!version) {
-        logger.error(`Version string '${latestTag}' is not in a valid format`);
-        return;
-    }
-    if (isMajor)
-        version = version.inc('major') || version;
-    if (isMinor)
-        version = version.inc('minor') || version;
-    if (isPatch)
-        version = version.inc('patch') || version;
-    logger.info(`New version is '${version.version}''`);
-    await octokit.repos.createRelease({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        tag_name: `v${version.version}`,
-        name: mergedPr.title,
-        body: mergedPr.body || '',
-        prerelease: false,
-        target_commitish: github.context.sha
-    });
-    logger.info('GitHub release created');
-    await prependToChangeLog(mergedPr.body || '', `v${version.version}`, mergedPr.url);
-    logger.info('Prepended to changelog');
-    outputs.setVersion(version.version);
-    outputs.setShouldPublish(true);
 }
 async function getMergedPr(owner, repo, sha) {
     logger.debug(`Getting merged pull request for: '${sha}''`);
